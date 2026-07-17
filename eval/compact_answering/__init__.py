f5420e4cdc5736d523e0a5dcf5964ce7ad5a0c1c
Jake B <burnitzjake@gmail.com>
2026-07-17 16:25:03 -0700

    feat: define byte-exact parity contract

--- eval/compact_answering/__init__.py
+++ eval/compact_answering/__init__.py
@@ -8,12 +8,26 @@ from .manifest import (
     load_manifest,
     validate_manifest,
 )
+from .parity import (
+    ParityMismatchError,
+    ParityRow,
+    ParityValidationError,
+    SupportingFact,
+    compare_parity_rows,
+    parse_parity_row,
+)
 
 __all__ = [
     "ManifestDriftError",
     "ManifestExistsError",
     "ManifestValidationError",
+    "ParityMismatchError",
+    "ParityRow",
+    "ParityValidationError",
+    "SupportingFact",
+    "compare_parity_rows",
     "create_manifest",
     "load_manifest",
+    "parse_parity_row",
     "validate_manifest",
 ]
